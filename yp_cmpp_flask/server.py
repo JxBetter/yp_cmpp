1bea5d19fbb5da2019de57971fa014d1033de0ec
jx <33095108+JxBetter@users.noreply.github.com>
2019-05-28 10:53:57 +0800

    fix

--- yp_cmpp_flask/server.py
+++ yp_cmpp_flask/server.py
@@ -1,25 +1,7 @@
-import time
-import pymysql
 from src.cmpp import Cmpp
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
-
-def connect_db(host, port, user, pwd, db_name):
-    connect = pymysql.Connect(
-        host=host,
-        port=port,
-        user=user,
-        passwd=pwd,
-        db=db_name,
-        charset='utf8'
-    )
-    cursor = connect.cursor()
-    return connect, cursor
-
-
-conn, cur = connect_db('119.23.216.161', 3306, 'guest', 'helloworld', 'yp_cmpp_database')
 
 
 @app.route('/')
@@ -29,10 +11,7 @@ def index():
 
 @app.route('/send', methods=['POST'])
 def send():
-    now = int(time.time())
-    timeStruct = time.localtime(now)
-    strTime = time.strftime("%Y-%m-%d %H:%M:%S", timeStruct)
-
+    print(request.form.get('username'))
     un = request.form.get('username')
     pwd = request.form.get('password')
     mobile = request.form.get('mobile')
@@ -41,31 +20,17 @@ def send():
     c = Cmpp('118.178.110.140', '30001', un, pwd, '0')
     f1 = c.connect_server()
     if f1 is False:
-        sql = 'INSERT INTO cmpp_log (ip, 用户名, 密码, 手机号, 短信内容, 发送时间, 发送状态) VALUE ("{}", "{}", "{}", "{}", "{}", "{}", "{}")'.format(
-            request.remote_addr, un, pwd, mobile, content, strTime, '失败')
-        cur.execute(sql)
-        conn.commit()
         return jsonify({'result': '连接cmpp服务器失败'})
     f2, conn_data = c.connect_application()
     if f2 is False:
-        sql = 'INSERT INTO cmpp_log (ip, 用户名, 密码, 手机号, 短信内容, 发送时间, 发送状态) VALUE ("{}", "{}", "{}", "{}", "{}", "{}", "{}")'.format(
-            request.remote_addr, un, pwd, mobile, content, strTime, '失败')
-        cur.execute(sql)
-        conn.commit()
         return jsonify({'result': '登陆cmpp失败，状态码为{}'.format(conn_data)})
     f3, send_data = c.send_message([mobile], content)
     if f3 is False:
-        sql = 'INSERT INTO cmpp_log (ip, 用户名, 密码, 手机号, 短信内容, 发送时间, 发送状态) VALUE ("{}", "{}", "{}", "{}", "{}", "{}", "{}")'.format(
-            request.remote_addr, un, pwd, mobile, content, strTime, '失败')
-        cur.execute(sql)
-        conn.commit()
         return jsonify({'result': '发送短信失败，状态码为{}'.format(send_data)})
-    sql = 'INSERT INTO cmpp_log (ip, 用户名, 密码, 手机号, 短信内容, 发送时间, 发送状态) VALUE ("{}", "{}", "{}", "{}", "{}", "{}", "{}")'.format(
-        request.remote_addr, un, pwd, mobile, content, strTime, '成功')
-    cur.execute(sql)
-    conn.commit()
     return jsonify({'result': 'ok'})
 
 
+
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5001)
+
